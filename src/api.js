24210e7ea3bc8446f308f1865583a8f75b188760
GabiFerraz <ferrazmgabi@gmail.com>
2022-05-21 19:15:57 -0300

    :sparkles: Configurando e Iniciando
    Configurando o dotenv, criando pastas, fazendo a configuração inicial do projeto e começando o desenvolvimento do requisito 1

--- src/api.js
+++ src/api.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const router = require('./routes');
 
+const handleError = require('./middlewares/handleError');
 // ...
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 // ...
+
+app.use(handleError);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
