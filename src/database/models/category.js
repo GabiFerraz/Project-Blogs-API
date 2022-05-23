b9f78f223bfd208f337f50d7a41b2ca0001074e2
GabiFerraz <ferrazmgabi@gmail.com>
2022-05-23 16:03:28 -0300

    :sparkles: Requisito 7
    Model category

---
+++ src/database/models/category.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  return Category;
+};
+
+module.exports = Category;
